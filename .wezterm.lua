41114eebf576b7b9c8f1612918425fad97941556
hhfs <38155672+hidehitofukushima@users.noreply.github.com>
2025-08-27 23:44:32 +0900

    -

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,7 @@ local config = {}
 
 config.initial_cols = 120
 config.initial_rows = 28
-config.font_size = 12
+config.font_size = 14
 config.font = wezterm.font 'Monaco'
 config.line_height = 1.2
 
