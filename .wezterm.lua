89baed3803e6c59eaa0cf47eb2bed80fac32c2f3
hhfs <38155672+hidehitofukushima@users.noreply.github.com>
2025-08-27 18:09:44 +0900

    -

--- .wezterm.lua
+++ .wezterm.lua
@@ -23,7 +23,7 @@ config.colors = {
   -- cursor and the cursor style is set to Block
   cursor_bg = '#705697',
   -- Overrides the text color when the current cell is occupied by the cursor
-  cursor_fg = '#FFFFFF',
+  cursor_fg = '#705697',
   -- Specifies the border color of the cursor when the cursor style is set to Block,
   -- or the color of the vertical or horizontal bar when the cursor style is set to
   -- Bar or Underline.
@@ -68,9 +68,9 @@ config.colors = {
     '#705697',
     '#705697',
   },
-
+	
   -- Arbitrary colors of the palette in the range from 16 to 255
-  indexed = { [136] = '#705697' },
+  indexed = { [136] = '#af8700' },
 
   -- Since: 20220319-142410-0fcdea07
   -- When the IME, a dead key or a leader key are being processed and are effectively
@@ -83,26 +83,24 @@ config.colors = {
   -- In copy_mode, the color of the active text is:
   -- 1. copy_mode_active_highlight_* if additional text was selected using the mouse
   -- 2. selection_* otherwise
-  copy_mode_active_highlight_bg = { Color = 'Red' },
+  copy_mode_active_highlight_bg = { Color = '#000000' },
   -- use `AnsiColor` to specify one of the ansi color palette values
   -- (index 0-15) using one of the names "Black", "Maroon", "Green",
   --  "Olive", "Navy", "Purple", "Teal", "Silver", "Grey", "Red", "Lime",
   -- "Yellow", "Blue", "Fuchsia", "Aqua" or "White".
   copy_mode_active_highlight_fg = { AnsiColor = 'Black' },
-  copy_mode_inactive_highlight_bg = { Color = 'red' },
+  copy_mode_inactive_highlight_bg = { Color = '#52ad70' },
   copy_mode_inactive_highlight_fg = { AnsiColor = 'White' },
 
-  quick_select_label_bg = { Color = 'red' },
+  quick_select_label_bg = { Color = 'peru' },
   quick_select_label_fg = { Color = '#ffffff' },
-  quick_select_match_bg = { AnsiColor = 'red' },
+  quick_select_match_bg = { AnsiColor = 'Navy' },
   quick_select_match_fg = { Color = '#ffffff' },
 
-  input_selector_label_bg = { AnsiColor = 'red' }, -- (*Since: Nightly Builds Only*)
+  input_selector_label_bg = { AnsiColor = 'Black' }, -- (*Since: Nightly Builds Only*)
   input_selector_label_fg = { Color = '#ffffff' }, -- (*Since: Nightly Builds Only*)
 
   launcher_label_bg = { AnsiColor = 'Black' }, -- (*Since: Nightly Builds Only*)
   launcher_label_fg = { Color = '#ffffff' }, -- (*Since: Nightly Builds Only*)
 }
-
 return config
-
