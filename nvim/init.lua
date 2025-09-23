4dc147bc4cc544f2d34e7f796ffb2896c0ef0026
hhfs <38155672+hidehitofukushima@users.noreply.github.com>
2025-09-24 04:26:16 +0900

    -

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,7 +34,6 @@ vim.pack.add({
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 	{ src = "https://github.com/supermaven-inc/supermaven-nvim" },
 	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
-	{ src = "https://github.com/ThePrimeagen/harpoon" },
 })
 
 -- -----------------------------------------------------------------------------
