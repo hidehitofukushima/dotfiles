ab7c0ca254314a1589a7832299528ee366a01733
hhfs <38155672+hidehitofukushima@users.noreply.github.com>
2025-09-25 23:29:57 +0900

    -

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,11 +34,51 @@ vim.pack.add({
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 	{ src = "https://github.com/supermaven-inc/supermaven-nvim" },
 	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
+	{ src = "https://github.com/3rd/image.nvim" },
 })
 
 -- -----------------------------------------------------------------------------
 -- 各機能の設定とキーマップ (configurations & keymaps)
 -- -----------------------------------------------------------------------------
+require("image").setup({
+  backend = "kitty", -- or "ueberzug" or "sixel"
+  processor = "magick_cli", -- or "magick_rock"
+  integrations = {
+    markdown = {
+      enabled = true,
+      clear_in_insert_mode = false,
+      download_remote_images = true,
+      only_render_image_at_cursor = false,
+      only_render_image_at_cursor_mode = "popup", -- or "inline"
+      floating_windows = false, -- if true, images will be rendered in floating markdown windows
+      filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+    },
+    neorg = {
+      enabled = true,
+      filetypes = { "norg" },
+    },
+    typst = {
+      enabled = true,
+      filetypes = { "typst" },
+    },
+    html = {
+      enabled = false,
+    },
+    css = {
+      enabled = false,
+    },
+  },
+  max_width = nil,
+  max_height = nil,
+  max_width_window_percentage = nil,
+  max_height_window_percentage = 50,
+  scale_factor = 1.0,
+  window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
+  window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "snacks_notif", "scrollview", "scrollview_sign" },
+  editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
+  tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+  hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
+})
 require("supermaven-nvim").setup({
   keymaps = {
     accept_suggestion = "<C-k>",
@@ -64,6 +104,8 @@ require("bufferline").setup()
 --
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
+keymap("n", "<C-d>", "<C-d>zz", opts)
+keymap("n", "<C-u>", "<C-u>zz", opts)
 keymap("n", "lkj", "<Plug>SlimeLineSend", opts)
 keymap("x", "lkj", "<Plug>SlimeRegionSend", opts) -- 選択範囲の送信
 keymap("n", ";lkj", "vip<Plug>SlimeRegionSend", opts) -- 段落の送信
---
+++ tmp.md
@@ -0,0 +1,1 @@
+![render loci images](/Users/fukushimahideto/Desktop/Projects_wgs/STJUDE/SuppTable11_SampleByMutMatrix/figure3_BCR.svg)
