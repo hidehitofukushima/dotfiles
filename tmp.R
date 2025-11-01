578fec45e7dd94d8d28b6a95f345884c6016537e
hhfs <38155672+hidehitofukushima@users.noreply.github.com>
2025-11-01 22:17:48 +0900

    -

--- tmp.R
+++ tmp.R
@@ -5,3 +5,24 @@ library(readxl)
 
 
 
+
+library(tidyverse)
+file <- "~/Desktop/Projects_Other/clinical_sequencing_workflow/database/README.txt"
+
+
+
+
+###########################################
+# README.txt
+###########################################
+head(iris)
+head(mtcars)
+head(mtcars, n = 10)
+tail(mtcars)
+tail(mtcars, n = 10)
+View(mtcars)
+View(mtcars, title = "View of mtcars")
+View(mtcars, title = "View of mtcars", page = 1)
+
+
+apply(mtcars, 2, function(x) mean(x))
