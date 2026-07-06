f04b5b5416498f054aa5e6dc6df80d03a6c46c58
hhfs <38155672+hidehitofukushima@users.noreply.github.com>
2026-07-06 10:37:56 +0900

    Update dotfiles

--- tmp.sh
+++ tmp.sh
@@ -5,3 +5,13 @@ hoge() {
 	echo $2
 }
 
+find .  -name '*.txt' 
+awk -F'\t' 'NR == 1 {
+  for (i = 1; i <= NF; i++) {
+    print i, $i
+  }
+}' input.tsv
+module use /usr/local/package/modulefiles
+module load hoge
+eval "$(~/miniconda3/bin/conda shell.bash hook)"
+export R_LIBS_USER=/home/ny1fh/R/hoge
